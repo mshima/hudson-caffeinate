48495add23554c84c46a3b01e0898e555984a239
Marcelo Shima <marceloshima@gmail.com>
2015-05-02 18:05:53 -0300

    Initial import

---
+++ src/main/java/br/net/shima/utils/hudson/CaffeinateRunListener.java
@@ -0,0 +1,31 @@
+package br.net.shima.utils.hudson;
+
+import hudson.Extension;
+import hudson.model.TaskListener;
+import hudson.model.AbstractBuild;
+import hudson.model.listeners.RunListener;
+import br.net.shima.utils.caffeinate.CaffeinateThread;
+
+@Extension
+public class CaffeinateRunListener extends RunListener<AbstractBuild<?, ?>> {
+
+	private CaffeinateThread caffeinate = new CaffeinateThread();
+
+	public CaffeinateRunListener() {
+	}
+
+	@Override
+	public void onStarted(AbstractBuild<?, ?> r, TaskListener listener) {
+		listener.getLogger().println("Starting caffeined for name " + r.getId());
+		this.caffeinate.cancelableCaffeinate(r.getId());
+		super.onStarted(r, listener);
+	}
+
+	@Override
+	public void onCompleted(AbstractBuild<?, ?> r, TaskListener listener) {
+		listener.getLogger().println("Canceling caffeined for name " + r.getId());
+		this.caffeinate.cancel(r.getId());
+		super.onCompleted(r, listener);
+	}
+
+}
